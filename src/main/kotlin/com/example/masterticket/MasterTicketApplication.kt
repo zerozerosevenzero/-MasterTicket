4256be4c89dc5519c96a29679c330fb021839f49
KingBlackCow <sgs1159@naver.com>
2023-05-20 23:49:02 +0900

    feat: first init

---
+++ src/main/kotlin/com/example/masterticket/MasterTicketApplication.kt
@@ -0,0 +1,11 @@
+package com.example.masterticket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MasterTicketApplication
+
+fun main(args: Array<String>) {
+    runApplication<MasterTicketApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/masterticket/MasterTicketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.masterticket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MasterTicketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
