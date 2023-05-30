ebe1ca9625b14bbf64c8463d827e3b6470e02bb5
KingBlackCow <sgs1159@naver.com>
2023-05-31 01:38:12 +0900

    refactor: BatchConfig 생성

--- src/main/kotlin/com/example/masterticket/MasterTicketApplication.kt
+++ src/main/kotlin/com/example/masterticket/MasterTicketApplication.kt
@@ -1,26 +1,9 @@
 package com.example.masterticket
 
-import org.springframework.batch.core.*
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
-import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.batch.core.listener.JobExecutionListenerSupport
-import org.springframework.batch.core.scope.context.ChunkContext
-import org.springframework.batch.core.step.tasklet.Tasklet
-import org.springframework.batch.item.ItemProcessor
-import org.springframework.batch.item.ItemReader
-import org.springframework.batch.item.ItemWriter
-import org.springframework.batch.item.support.ListItemReader
-import org.springframework.batch.repeat.RepeatStatus
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.stereotype.Component
 
-@EnableBatchProcessing
 @EnableJpaAuditing
 @SpringBootApplication
 class MasterTicketApplication
--- src/main/kotlin/com/example/masterticket/config/BatchConfig.kt
+++ src/main/kotlin/com/example/masterticket/config/BatchConfig.kt
@@ -1,4 +1,19 @@
 package com.example.masterticket.config
 
+import org.springframework.batch.core.configuration.JobRegistry
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@EnableBatchProcessing
+@Configuration
 class BatchConfig {
+
+    @Bean
+    fun jobRegistryBeanPostProcessor(jobRegistry: JobRegistry?): JobRegistryBeanPostProcessor? {
+        val jobRegistryBeanPostProcessor = JobRegistryBeanPostProcessor()
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry!!)
+        return jobRegistryBeanPostProcessor
+    }
 }
