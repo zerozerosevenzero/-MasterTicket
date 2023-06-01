eb356c1d613b524d964ee442d386e275fe0276f2
KingBlackCow <sgs1159@naver.com>
2023-06-02 01:14:25 +0900

    refactor: optimize import

--- src/main/kotlin/com/example/masterticket/config/KakaoTalkMessageConfig.kt
+++ src/main/kotlin/com/example/masterticket/config/KakaoTalkMessageConfig.kt
@@ -1,7 +1,6 @@
 package com.example.masterticket.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.stereotype.Component
 
 @Component
