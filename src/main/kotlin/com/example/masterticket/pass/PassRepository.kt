b19b013f29052eea4269984abece9326c0abd2a5
KingBlackCow <sgs1159@naver.com>
2023-06-04 20:00:09 +0900

    refactor: 안쓰는 코드 제거

--- src/main/kotlin/com/example/masterticket/pass/PassRepository.kt
+++ src/main/kotlin/com/example/masterticket/pass/PassRepository.kt
@@ -1,21 +1,6 @@
 package com.example.masterticket.pass
 
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
-import java.time.LocalDateTime
-import javax.transaction.Transactional
 
 interface PassRepository : JpaRepository<Pass, Long>, PassRepositoryCustom {
-//    @Transactional
-//    @Modifying
-//    @Query(
-//        value = "UPDATE Pass p " +
-//                "SET p.remainingCount = :remainingCount," +
-//                "p.modifiedAt = CURRENT_TIMESTAMP " +
-//                "WHERE p.id = :id"
-//    )
-//    fun updateRemainingCount(id: Long?, remainingCount: Int?): Int
 }
